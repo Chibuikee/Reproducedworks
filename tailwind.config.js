d930ce27e70860ed57482fb58cd0733e9300fb39
Chibuike Ewenike <sopewenike@yahoo.com>
2023-08-02 12:15:34 +0100

    sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primarycolor: "#090E14",
+      text: "white",
+      sidebargray: "#111926",
+      sidebargraylight: "#D8DCE0",
+      sidebargraybackground: "#373E4A",
+      success: "#00C896",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
